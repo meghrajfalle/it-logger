b7ca877f4bee14ba713d07068e841888b6806787
meghrajfalle <meghraj.falle@appdirect.com>
2020-09-28 03:44:38 +0530

    Branch2: Added SearchBar Component insided component/layout and initialized it from Materialize. Tweaked the properties to change it to ReactJS, like className, htmlFor. Added marginBottom. Added Logs and LogItem components in components/logs folder. Called getlogs from useEffect of Logs which will fetch from /logs API and the result will then be converted to json data.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min';
 import './App.css';
@@ -8,7 +11,14 @@ const App = () => {
     //Init Materialize Js
     M.AutoInit();
   });
-  return <div className='App'>My App</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
---
+++ src/components/logs/Logs.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getlogs();
+  }, []);
+
+  const getlogs = async () => {
+    setLoading(true);
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <h4>Loading...</h4>;
+  }
+
+  return (
+    <div>
+      <ul className='collection-with-header'>
+        <li className='collection-header'>
+          <h4 className='center'>System Logs</h4>
+        </li>
+        {!loading && logs.length === 0 ? (
+          <p className='center'> No logs to show...</p>
+        ) : (
+          logs.map(log => <li>{log.message}</li>)
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Logs;
