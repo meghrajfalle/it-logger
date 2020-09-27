4bdb491116949407d23ab2a5306beed6cacd2416
meghrajfalle <meghraj.falle@appdirect.com>
2020-09-28 03:15:19 +0530

    Branch1: Added materialize dependency. Removed the comments, changed App.js to functional component and initialized materialize by importing and using autoinit function. Removed existing files lik index.css, svg, test.js and cleared content of App.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  useEffect(() => {
+    //Init Materialize Js
+    M.AutoInit();
+  });
+  return <div className='App'>My App</div>;
+};
 
 export default App;
