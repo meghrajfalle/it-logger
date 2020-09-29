d473fc016142d48b7a211d15a2fa3a9c90ca42a0
meghrajfalle <meghraj.falle@appdirect.com>
2020-09-30 00:03:14 +0530

    Branch4: Added + button and add person and view technicians button in AddBtn.js. Added AddLogModal which will trigger by + button and it will be a form which will take input- a log message a selector and attention flag in AddLogModal.js. On submit of Form the function will be called see if the tech is not selected or the log message is not added, this will create an alert message with the help of toast method from Materialize

---
+++ src/components/layout/AddBtn.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const AddBtn = () => {
+  return (
+    <div className='fixed-action-btn'>
+      <a
+        href='#add-log-modal'
+        className='btn-floating btn-large waves-effect blue darken-2 modal-trigger'
+      >
+        <i className='large material-icons'>add</i>
+      </a>
+      <ul>
+        <li>
+          <a
+            href='#tech-list-modal'
+            className='btn-floating waves-effect green modal-trigger'
+          >
+            {' '}
+            <i className='material-icons'>person</i>
+          </a>
+        </li>
+        <li>
+          <a
+            href='#tech-modal'
+            className='btn-floating  waves-effect red modal-trigger'
+          >
+            {' '}
+            <i className='material-icons'>person_add</i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AddBtn;
