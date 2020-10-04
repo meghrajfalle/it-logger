0d4e75dabc6e3dd92195b3eca7cdb4c5a98f6164
meghrajfalle <meghraj.falle@appdirect.com>
2020-10-04 11:15:25 +0530

    Branch14: Added TechSelectOptions that will show the list of technicians when we click on the select options from the state based on the function getTechs() avl from techActions.js. Added the TechSelectOptions in AddLogModal and EditLogModal

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
+import TechListModal from '../techs/TechListModal';
 
 const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
@@ -58,9 +60,7 @@ const AddLogModal = ({ addLog }) => {
               <option value='' disabled>
                 Select Technician
               </option>
-              <option value='John Doe'>John Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
